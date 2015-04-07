52857de5fac4a8e4cc412a3575651dff9df4d810
Jorge Yagüe París <jorgeyp@gmail.com>
2015-04-08 01:09:13 +0200

    Add section titles.

--- mobile/src/main/java/com/jorgeyp/brewtutor/MainActivity.java
+++ mobile/src/main/java/com/jorgeyp/brewtutor/MainActivity.java
@@ -67,6 +67,12 @@ public class MainActivity extends Activity
             case 3:
                 mTitle = getString(R.string.title_section3);
                 break;
+            case 4:
+                mTitle = getString(R.string.title_section4);
+                break;
+            case 5:
+                mTitle = getString(R.string.title_section5);
+                break;
         }
     }
 
